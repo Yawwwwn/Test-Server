de19f421234afa9924e1297a8561560e25fff4b5
Kun <169256508@qq.com>
2026-01-29 12:39:20 +0800

    cleanup: remove unused components/icons; strip project to minimal Vue 3 + Vite; retain GitHub workflow

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
-<script setup lang='ts'>
-
-</script>
-
 <template>
-  <RouterView/>
+  <div id="app">
+    <h1>Vue 3 + Vite 项目</h1>
+    <p>这是一个干净的 Vue 3 项目模板</p>
+  </div>
 </template>
 
-<style scoped>
+<script setup lang="ts"></script>
 
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
